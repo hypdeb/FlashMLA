d4ac815a785e612962edb6625577bb573dc2dd29
jdebache <jdebache@nvidia.com>
2025-08-19 16:10:30 +0000

    fixed one more issue in combine kernel

--- csrc/sm90/kernels/mla_combine.h
+++ csrc/sm90/kernels/mla_combine.h
@@ -2,5 +2,5 @@
 
 #include "params.h"
 
-template<typename ElementT>
+template<typename ElementT, int HeadDimV>
 void run_flash_mla_combine_kernel(Flash_fwd_mla_params &params, cudaStream_t stream);
